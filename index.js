67891a10f7d351715687e688bc5717862f8133fe
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-04-26 19:14:08 -0500

    Add Event Listeners on buttons

---
+++ index.js
@@ -0,0 +1,4 @@
+(function imageSlider(btnOneEL, btnTwoEL) {
+	btnOneEL.addEventListener('click', () => {});
+	btnTwoEL.addEventListener('click', () => {});
+})(document.getElementById('left-btn'), document.getElementById('right-btn'));
